99c72e1849014c705e1cd4219a06b9ec2f130e2c
Mateus Cichelero <mateuscichelero@outlook.com>
2024-11-02 12:50:16 -0300

    v2-adiciona-e-remove-estoque

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,18 +10,29 @@ public class Program{
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); //variavel scanner
 
-        Product product = new Product();
+        Product product = new Product(); //cria o produto
 
         System.out.println("Enter product data(name, price, quantity in stock): ");
-        product.name = sc.nextLine();
-        product.price = sc.nextDouble();
-        product.quantity = sc.nextInt();
+        product.name = sc.nextLine(); //nome
+        product.price = sc.nextDouble(); //preço
+        product.quantity = sc.nextInt(); //quantidade
 
-        System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+        System.out.println("Product data: " + product); //imprime todas as informações
 
-        sc.close();
+        System.out.println("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt(); //quantidade a ser adicionada
+        product.addProducts(quantity); //adiciona no estoque a quantidade informada
 
+        System.out.println("Updated data: " + product); //imprime as informaçoes atualizadas
+
+        System.out.println("Enter the number of products to be remove from stock: ");
+        quantity = sc.nextInt(); //quantidade a ser removida
+        product.removeProducts(quantity); //remove do estoque a quantidade informada
+
+        System.out.println("Updated data: " + product); //imprime as informaçoes atualizadas
+
+        sc.close(); //fecha a variavel scanner
     }
 }
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -6,15 +6,19 @@ public class Product {
     public double price;
     public int quantity;
 
-    public double totalValueInStock(){
+    public double totalValueInStock(){ //calcula o valor em dolares total em estoque
         return price * quantity;
     }
 
-    public void addProducts(int quantity){
+    public void addProducts(int quantity){ //adiciona quantidade em estoque
         this.quantity += quantity;
     }
 
-    public void removeProducts(int quantity ){
+    public void removeProducts(int quantity ){ //remove quantidade em estoque
         this.quantity -= quantity;
+    }
+
+    public String toString() { // imprime todas as informações somente chamando a classe Product lá pelo Program
+        return name + ", $" + String.format("%.2f", price) + ", " + quantity + " units, Total: $" + String.format("%.2f", totalValueInStock());
     }
 }
